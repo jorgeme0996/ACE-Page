c76aba73d86e43b5ab59c79f77cee21fe5077583
Jorge Martínez <jorgeme0996@gmail.com>
2019-05-22 16:15:48 -0500

    Se sube el front de la pagina de ACE

---
+++ app.js
@@ -0,0 +1,18 @@
+  
+const   express = require('express'),
+        bodyParser = require('body-parser'),
+        request = require('request'),
+        nodemailer = require('nodemailer'),
+        app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log("Esta vivo")
+}); 
